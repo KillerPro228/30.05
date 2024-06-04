7b8a32784a9348e0edf1dd14465f34528f3a3c3b
KillerPro228 <vy9yyfkj6fwz@gmail.com>
2024-06-04 23:56:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const person = {
+  name: 'Jack',
+  age: 30,
+  gender: 'male',
+};
+
+person.name = 'Jack' 
+person.age = 30
+person.gender = 'male'
+
+console.log(`Имя: ${person.name}, Возраст: ${person.age}, Пол: ${person.gender}`)
